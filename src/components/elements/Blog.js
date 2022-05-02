84c14a5a06469103d310e3b3ca10ba888cc8de88
DhruvSantilal <73472888+DhruvSantilal@users.noreply.github.com>
2022-05-02 22:32:05 +0100

    first commit

---
+++ src/components/elements/Blog.js
@@ -0,0 +1,59 @@
+import React from "react";
+import ScrollAnimation from "react-animate-on-scroll";
+import { Link } from "react-router-dom";
+
+function Blog({ blogData }) {
+  const {
+    // id,
+    category,
+    title,
+    date,
+    author,
+    image,
+    frontend,
+    backend,
+    api,
+    live,
+    githubURL,
+  } = blogData;
+
+  return (
+    <ScrollAnimation
+      animateIn="fadeInUp"
+      animateOut="fadeInOut"
+      animateOnce={true}
+    >
+      <div className="blog-item rounded bg-white shadow-dark">
+        <div className="thumb">
+          <a>
+            <span className="category">{category}</span>
+          </a>
+
+          <img src={image} alt="blog-title" />
+        </div>
+        <div className="details">
+          <h4 className="my-0 title">{title}</h4>
+          <ul className="list-inline meta mb-0 mt-2">
+            <li className="list-inline-item">{date}</li>
+            <li className="list-inline-item">{author}</li>
+          </ul>
+          <p>{frontend}</p>
+          <p>{backend}</p>
+          <p>{api}</p>
+          <div className="d-flex justify-content-between  my-2">
+            {live && <Link className="btn btn-default btn-md">Live</Link>}
+            <a
+              target="_blank"
+              href={githubURL}
+              className="btn btn-default btn-md"
+            >
+              Source
+            </a>
+          </div>
+        </div>
+      </div>
+    </ScrollAnimation>
+  );
+}
+
+export default Blog;
---
+++ src/components/sections/Contact.js
@@ -0,0 +1,194 @@
+import React, { useState } from "react";
+import ScrollAnimation from "react-animate-on-scroll";
+import Pagetitle from "../elements/Pagetitle";
+import emailjs from "emailjs-com";
+
+function Contact() {
+  const [formdata, setFormdata] = useState({
+    from_name: "",
+    from_email: "",
+    subject: "",
+    message: "",
+  });
+
+  const [error, setError] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setDisabled(true);
+    if (!formdata.from_name) {
+      setError(true);
+      setMessage("Name is required");
+    } else if (!formdata.from_email) {
+      setError(true);
+      setMessage("Email is required");
+    } else if (!formdata.subject) {
+      setError(true);
+      setMessage("Subject is required");
+    } else if (!formdata.message) {
+      setError(true);
+      setMessage("Message is required");
+    } else {
+      emailjs
+        .sendForm(
+          "service_1f448bn",
+          "template_ewi4wxa",
+          event.target,
+          "user_LZOdjrmcYSEO8ME4FRSLK"
+        )
+        .then(
+          (result) => {
+            setMessage("You message has been sent!!!");
+            setError(false);
+            setFormdata({
+              from_name: "",
+              from_email: "",
+              subject: "",
+              message: "",
+            });
+          },
+          (error) => {
+            setDisabled(false);
+            setMessage("You message was not sent, something went wrong!");
+            setError(true);
+            console.log(error.text);
+          }
+        );
+    }
+  };
+
+  const handleChange = (event) => {
+    setFormdata({
+      ...formdata,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
+
+  const handleAlerts = () => {
+    if (error && message) {
+      return <div className="alert alert-danger mt-4">{message}</div>;
+    } else if (!error && message) {
+      return <div className="alert alert-success mt-4">{message}</div>;
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <section id="contact">
+      <div className="container">
+        <Pagetitle title="Get in Touch" />
+
+        <div className="row">
+          <div className="col-md-4">
+            <div className="contact-info">
+              <ScrollAnimation
+                animateIn="fadeInUp"
+                animateOut="fadeInOut"
+                animateOnce={true}
+              >
+                <h3>Let's talk about everything!</h3>
+              </ScrollAnimation>
+              <ScrollAnimation
+                animateIn="fadeInUp"
+                animateOut="fadeInOut"
+                animateOnce={true}
+              >
+                <p>
+                  Don't like forms? Send me an{" "}
+                  <a target="_blank" href="mailto:dhruvpatel2404@hotmail.co.uk">
+                    dhruvpatel2404@hotmail.co.uk
+                  </a>
+                  . 👋
+                </p>
+              </ScrollAnimation>
+            </div>
+          </div>
+
+          <div className="col-md-8">
+            <form
+              id="contact-form"
+              className="contact-form mt-6"
+              onSubmit={submitHandler}
+            >
+              <div className="row">
+                <div className="column col-md-6">
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="from_name"
+                      id="from_name"
+                      placeholder="Your name"
+                      onChange={handleChange}
+                      value={formdata.name}
+                    />
+                  </div>
+                </div>
+
+                <div className="column col-md-6">
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="from_email"
+                      name="from_email"
+                      placeholder="Email address"
+                      onChange={handleChange}
+                      value={formdata.email}
+                    />
+                  </div>
+                </div>
+
+                <div className="column col-md-12">
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="subject"
+                      name="subject"
+                      placeholder="Subject"
+                      onChange={handleChange}
+                      value={formdata.subject}
+                    />
+                  </div>
+                </div>
+
+                <div className="column col-md-12">
+                  <div className="form-group">
+                    <textarea
+                      name="message"
+                      id="InputMessage"
+                      className="form-control"
+                      rows="5"
+                      placeholder="Message"
+                      onChange={handleChange}
+                      value={formdata.message}
+                    ></textarea>
+                  </div>
+                </div>
+              </div>
+
+              {!disabled && (
+                <button
+                  type="submit"
+                  name="submit"
+                  id="submit"
+                  value="Submit"
+                  className="btn btn-default"
+                >
+                  Send Message
+                </button>
+              )}
+            </form>
+            {handleAlerts()}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
